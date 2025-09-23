a12cdcee799e852b1ec7f0e2b2aa4dca7456ecb4
xentinelsolutions <xentinelsolutions@gmail.com>
2025-09-23 07:15:31 +0000

    feat: add basic view and update vapor build process

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks: {
-                    vendor: ['vue', 'vue-router', 'vuex'],
+                    vendor: ['vue', 'vue-router', 'pinia'],
                 },
             },
         },
